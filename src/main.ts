bc261042d3ac2c773b75222caf7168608dd90e6e
genghenggao <genghenggao@outlook.com>
2023-10-09 09:59:36 +0800

    finish newslist

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
  * @version: v1.0.0
  * @Date: 2023-10-08 23:08:55
  * @LastEditors: henggao
- * @LastEditTime: 2023-10-09 09:43:49
+ * @LastEditTime: 2023-10-09 09:58:13
  */
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -14,5 +14,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 // 引入bootstrap-icons图标
 import "bootstrap-icons/font/bootstrap-icons.css"
+import ElementPlus from "element-plus"
+import "element-plus/dist/index.css"
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@
  * @version: v1.0.0
  * @Date: 2023-10-08 23:08:55
  * @LastEditors: henggao
- * @LastEditTime: 2023-10-09 09:06:33
+ * @LastEditTime: 2023-10-09 09:51:57
  */
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomePage from '@/components/HomePage.vue'
@@ -15,13 +15,14 @@ const routes: Array<RouteRecordRaw> = [
     component: HomePage
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/newslist',
+    name: 'newslist',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../components/news/NewsList.vue')
   },
+  
 ]
 
 const router = createRouter({
